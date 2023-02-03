128631aeb309635a5bde126ef691e510119e85aa
Fabbiano1 <fabbianolorenzo2234@gmail.com>
2023-02-03 17:25:19 +0100

    Versione0

---
+++ ijfjfojfog/src/test.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ * <U> classe test</U>
+ * @author <Mark> Lorenzo </Mark>
+ * 
+ *
+ */
+public class test {
+   private String nome;
+
+	public test (String nome) {
+	super();
+	this.nome = nome;
+}
+
+
+	
+
+	@Override
+	public String toString() {
+		return "test [nome=" + nome + "]";
+	}
+
+
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
